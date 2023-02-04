e909e9d397a530691c8f7b9400e7a39e20c4cb5d
slackintime <slackintime@gmail.com>
2023-02-04 10:35:03 -0600

    test complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let revStr = "";
+ revStr = str.split('').reverse().join("");
+ return revStr;
 };
 
 // Do not edit below this line
