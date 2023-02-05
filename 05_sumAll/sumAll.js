995379d465d23ca038c5b918aebfccf6ba2b0bb0
slackintime <slackintime@gmail.com>
2023-02-05 14:38:40 -0600

    test completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+  let allTotal = 0;
+    if(!Number(first) || !Number(last)) { return "ERROR"};
+    if(first <= 0 || last <= 0) { return "ERROR"};
+    if(first > last) {
+      let switchNumbers = first;
+      first = last;
+      last = switchNumbers;
+  }
+  if(Number.isInteger(first) && Number.isInteger(last)) {
+      for(let i = first; i <= last; i++) {
+      allTotal += i;
+      };
+      return allTotal;
+  } else {
+      return "ERROR";
+  }
+console.log('test');
+console.log(allTotal);
 };
 
 // Do not edit below this line
